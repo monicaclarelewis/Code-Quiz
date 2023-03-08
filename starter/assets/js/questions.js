9400dbd5136fbba4c46e83c2fec8f7f4665e5167
monicaclarelewis <118432326+monicaclarelewis@users.noreply.github.com>
2023-03-08 12:06:16 +0000

    question layout updated and first question comes up when start button is clicked

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,47 +1,65 @@
-var questions = [
+var highscores = document.querySelector('.scores');
+var timer = document.querySelector('#time');
+var startButton = document.querySelector('.start');
+var revealQuestion = document.querySelector('#questions');
+var questionTitle = document.querySelector('#question-title');
+var question = document.getElementById('choices'); 
+var endScreen = document.querySelector('#end-screen');
+var finalScore = document.querySelector('#final-score');
+var initials = document.querySelector('#initials'); 
+var feedback = document.querySelector('#feedback');
+
+
+
+var Questions = [
     { 
         question: "What would 'pwd' do if typed into your terminal?", 
-        answers: [
-            { text: "Tell you your password", correct: false },
-            { text: "Tell you where you are located in the file system" , correct: true },
-            { text: "Makes a new repository", correct: false },
-            { text: "Nothing", correct: false }
-        ]
+        choices: [
+            "Tell you your password",
+            "Tell you where you are located in the file system",
+            "Makes a new repository",
+            "Nothing",
+        ],
+        Answer: "Tell you where you are located in the file system"
     },
     { 
         question: "What does CSS stand for?", 
-        answers: [
-            { text: "Coding Styled Sheets", correct: false },
-            { text: "Colour Styling Sheet", correct: false },
-            { text: "Cascading Style Sheets", correct: true },
-            { text: "Cascading Style Section", correct: false }
-        ]
+        choices: [
+            "Coding Styled Sheets",
+            "Colour Styling Sheet",
+            "Cascading Style Sheets",
+            "Cascading Style Section",
+        ],
+        Answer: "Cascading Style Sheets"
     },
     { 
         question: "Which syntax can be used to terminate a loop, switch or label statement?", 
-        answers: [
-            { text: "Break", correct: true },
-            { text: "Stop", correct: false },
-            { text: "End", correct: false },
-            { text: "Exit", correct: false }
-        ]
+        choices: [
+            "Break",
+            "Stop",
+            "End",
+            "Exit",
+        ],
+        Answer: "Break"
     },
     { 
         question: "Which symbol is used to select an HTML ID attribute in CSS?", 
-        answers: [
-            { text: "!", correct: false },
-            { text: "#", correct: true },
-            { text: ".", correct: false },
-            { text: "+", correct: false }
-        ]
+        choices: [
+            "!",
+            "#",
+            ".",
+             "+",
+        ],
+        Answer: "#"
     },
     { 
         question: "What tag is used to link a Javascript file to an HTML file?",
-        answers: [
-            { text: "<connect>", correct: false },
-            { text: "<join>", correct: false },
-            { text: "<link>", correct: false },
-            { text: "<script>", correct: true }
-        ]
+        choices: [
+            "<connect>",
+            "<join>",
+            "<link>",
+            "<script>",
+        ],
+        Answer: "<script>"
     },
 ];
--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -1,23 +1,43 @@
 
 //Timer
 var timer = document.querySelector('#time');
+// Scores
+var highscores = document.querySelector('.scores'); 
 // Buttons
 var startButton = document.querySelector("#start");
 //Start screen
 var startScreen = document.querySelector('.start')
-// Scores
-var highscores = document.querySelector('.scores'); 
+// Questions
+var revealQuestion = document.querySelector('#questions');
+var questionTitle = document.querySelector('#question-title');
+var question = document.querySelector('choices'); 
+var Currentquestion = "";
 
 var seconds = 60;
 timer.textContent = 60;
 
+var Questiontrack = 0;
+
 // Start button
 startButton.addEventListener("click",  function () {
-  // removes page wrapper
-startScreen.setAttribute("style", "display: none;");
-//showQuestions();
+startScreen.setAttribute("style", "display: none;"); // removes page wrapper
+showQuestions();
 startTimer ();
 });
+
+//Question function
+function showQuestions () { 
+revealQuestion.setAttribute("class", "");
+questionTitle.textContent = Questions[Questiontrack].question
+// adds questions, choices & answer
+for (var i = 0; i < Questions[Questiontrack].choices.length; i++) {
+var btn = document.createElement('button');
+// button content 
+btn.textContent = Questions[Questiontrack].choices[i]
+questions.appendChild(btn);
+btn.setAttribute("id", "options" + i)}
+}
+
 
 // Start timer
 function startTimer () {
