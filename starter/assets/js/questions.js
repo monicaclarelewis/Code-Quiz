fa7b3cc355c0816c6b63cb8d6a9beb6e95cacaaa
monicaclarelewis <118432326+monicaclarelewis@users.noreply.github.com>
2023-03-14 15:54:51 +0000

    event listener for clicking the answer

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,65 +1,59 @@
-var highscores = document.querySelector('.scores');
-var timer = document.querySelector('#time');
-var startButton = document.querySelector('.start');
-var revealQuestion = document.querySelector('#questions');
-var questionTitle = document.querySelector('#question-title');
-var question = document.getElementById('choices'); 
-var endScreen = document.querySelector('#end-screen');
-var finalScore = document.querySelector('#final-score');
-var initials = document.querySelector('#initials'); 
-var feedback = document.querySelector('#feedback');
+// var highscores = document.querySelector('.scores');
+// var timer = document.querySelector('#time');
+// var startButton = document.querySelector('.start');
+// var revealQuestion = document.querySelector('#questions');
+// var questionTitle = document.querySelector('#question-title');
+// var question = document.getElementById('choices'); 
+// var endScreen = document.querySelector('#end-screen');
+// var finalScore = document.querySelector('#final-score');
+// var initials = document.querySelector('#initials'); 
+// var feedback = document.querySelector('#feedback');
 
 
-
-var Questions = [
-    { 
-        question: "What would 'pwd' do if typed into your terminal?", 
-        choices: [
-            "Tell you your password",
-            "Tell you where you are located in the file system",
-            "Makes a new repository",
-            "Nothing",
-        ],
-        Answer: "Tell you where you are located in the file system"
+const Questions = [
+    { question: "What would 'pwd' do if typed into your terminal?", 
+    choices: [
+        "Tell you your password",
+        "Tell you your location in the file system",
+        "Makes a new repository",
+        "Nothing",
+    ],
+    Answer: "Tell you where you are located in the file system"
     },
-    { 
-        question: "What does CSS stand for?", 
-        choices: [
-            "Coding Styled Sheets",
-            "Colour Styling Sheet",
-            "Cascading Style Sheets",
-            "Cascading Style Section",
-        ],
-        Answer: "Cascading Style Sheets"
+    { question: "What does CSS stand for?", 
+    choices: [
+        "Coding Styled Sheets",
+        "Colour Styling Sheet",
+        "Cascading Style Sheets",
+        "Cascading Style Section",
+    ],
+    Answer: "Cascading Style Sheets"
     },
-    { 
-        question: "Which syntax can be used to terminate a loop, switch or label statement?", 
-        choices: [
-            "Break",
-            "Stop",
-            "End",
-            "Exit",
-        ],
-        Answer: "Break"
+    { question: "Which syntax can be used to terminate a loop, switch or label statement?", 
+    choices: [
+        "Break",
+        "Stop",
+        "End",
+        "Exit",
+    ],
+    Answer: "Break"
     },
-    { 
-        question: "Which symbol is used to select an HTML ID attribute in CSS?", 
-        choices: [
-            "!",
-            "#",
-            ".",
-             "+",
-        ],
-        Answer: "#"
+    { question: "Which symbol is used to select an HTML ID attribute in CSS?", 
+    choices: [
+        "!",
+        "#",
+        ".",
+            "+",
+    ],
+    Answer: "#"
     },
-    { 
-        question: "What tag is used to link a Javascript file to an HTML file?",
-        choices: [
-            "<connect>",
-            "<join>",
-            "<link>",
-            "<script>",
-        ],
-        Answer: "<script>"
+    { question: "What tag is used to link a Javascript file to an HTML file?",
+    choices: [
+        "<connect>",
+        "<join>",
+        "<link>",
+        "<script>",
+    ],
+    Answer: "<script>"
     },
 ];
--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -1,8 +1,6 @@
 
 //Timer
 var timer = document.querySelector('#time');
-// Scores
-var highscores = document.querySelector('.scores'); 
 // Buttons
 var startButton = document.querySelector("#start");
 //Start screen
@@ -10,13 +8,22 @@ var startScreen = document.querySelector('.start')
 // Questions
 var revealQuestion = document.querySelector('#questions');
 var questionTitle = document.querySelector('#question-title');
-var question = document.querySelector('choices'); 
-var Currentquestion = "";
+var question = document.getElementById('choices'); 
+var answer = document.getElementById('Answer');
+//Quiz end
+var endScreen = document.querySelector('#end-screen');
+var finalScore = document.querySelector('#final-score');
+var initials = document.querySelector('#initials'); 
+var feedback = document.querySelector('#feedback');
+var highscores = document.querySelector('#highscores'); 
 
 var seconds = 60;
-timer.textContent = 60;
 
 var Questiontrack = 0;
+
+var userScore = 0;
+
+var Currentquestion = "";
 
 // Start button
 startButton.addEventListener("click",  function () {
@@ -25,30 +32,57 @@ showQuestions();
 startTimer ();
 });
 
-//Question function
-function showQuestions () { 
-revealQuestion.setAttribute("class", "");
-questionTitle.textContent = Questions[Questiontrack].question
-// adds questions, choices & answer
-for (var i = 0; i < Questions[Questiontrack].choices.length; i++) {
-var btn = document.createElement('button');
-// button content 
-btn.textContent = Questions[Questiontrack].choices[i]
-questions.appendChild(btn);
-btn.setAttribute("id", "options" + i)}
-}
-
-
-// Start timer
+// Timer
 function startTimer () {
   var Countdown = setInterval (function () {
       timer.textContent = seconds
       seconds--;
-  if (seconds == 0) {
+  if (seconds < 0) {
       // clears countdown
       clearInterval(Countdown); 
       timer.textContent = 60
       alert("You have run out of time!")
   }
   }, 1000) 
-}
+};
+
+//Questions
+function showQuestions () { 
+revealQuestion.setAttribute("class", "");
+questionTitle.textContent = Questions[Questiontrack].question;
+// adds questions, choices & answer
+for (var i = 0; i < Questions[Questiontrack].choices.length; i++) {
+// sets choices as button
+var btn = document.createElement('button');
+btn.textContent = Questions[Questiontrack].choices[i];
+question.appendChild(btn);
+btn.setAttribute("id", "options" + i)}
+};
+
+//Answering
+choices.addEventListener("click", function(event) {
+  if (event === answer) { // DOESN'T WORK 
+    Correct();
+  } else {
+    seconds -= 10;
+    Incorrect();
+  }
+});
+
+//Correct answer
+function Correct () {
+  feedback.textContent = "Correct!"
+  feedback.setAttribute("class", "")
+};
+//Incorrect answer
+function Incorrect () {
+  feedback.textContent = "Wrong!"
+  feedback.setAttribute("class", "")
+};
+
+//TODO
+//Next questions attribute
+//End screen shows
+//Set final time as score + show on screen
+//Allow user to enter initials add score as highscore
+//Set highscores on the highscore page
