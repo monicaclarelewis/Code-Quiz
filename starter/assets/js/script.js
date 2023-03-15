da16a5b1b4d07d0c8f0675ac240e2a18964db507
monicaclarelewis <118432326+monicaclarelewis@users.noreply.github.com>
2023-03-15 09:47:32 +0000

    add timer flow and questino flow

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -16,7 +16,7 @@ var initials = document.querySelector('#initials');
 var feedback = document.querySelector('#feedback');
 var highscores = document.querySelector('#highscores'); 
 
-var seconds = 60;
+var seconds = Questions.length * 15;
 
 var Questiontrack = 0;
 
@@ -24,50 +24,107 @@ var userScore = 0;
 
 var Currentquestion = "";
 
+var timerId;
 // Start button
 startButton.addEventListener("click",  function () {
 startScreen.setAttribute("style", "display: none;"); // removes page wrapper
+
+timerId = setInterval(startTimer, 1000)
+
+timer.textContent = seconds;
 showQuestions();
-startTimer ();
+
 });
 
 // Timer
 function startTimer () {
-  var Countdown = setInterval (function () {
-      timer.textContent = seconds
+    timer.textContent = seconds
       seconds--;
   if (seconds < 0) {
       // clears countdown
-      clearInterval(Countdown); 
-      timer.textContent = 60
-      alert("You have run out of time!")
+    endGame()
   }
-  }, 1000) 
 };
 
 //Questions
 function showQuestions () { 
+
 revealQuestion.setAttribute("class", "");
-questionTitle.textContent = Questions[Questiontrack].question;
-// adds choices
-for (var i = 0; i < Questions[Questiontrack].choices.length; i++) {
-// sets choices as button
-var btn = document.createElement('button');
-btn.textContent = Questions[Questiontrack].choices[i];
-choices.appendChild(btn);
-btn.setAttribute("id", "options" + i)}
+var current = Questions[Questiontrack]
+
+questionTitle.textContent = current.question
+
+choices.innerHTML = ''
+
+for (i = 0; i < current.choices.length; i++) {
+
+ var choice = current.choices[i]
+
+ var choiceBtn = document.createElement('button')
+
+ choiceBtn.setAttribute('class', 'choice')
+  choiceBtn.setAttribute('value', choice)
+
+  choiceBtn.textContent = i + 1 + '. ' + choice;
+
+  choices.appendChild(choiceBtn)
+}
 };
 
-//Answering
-choices.addEventListener("click", function(event) {
-  if ((this.value === Questions[Questiontrack].Answer)) { // DOESN'T WORK 
-    Correct();
-  } else {
-    seconds -= 10;
-    Incorrect();
-  }
-});
+// //Answering
+// choices.addEventListener("click", function(event) {
+//   if ((event.target === Questions[Questiontrack].Answer)) { // DOESN'T WORK 
+//     Correct();
+//   } 
+//   else {
+//     seconds -= 10;
+//     Incorrect();
+//   }
+// });
 
+function answerClicked(event) {
+  // get response from user event (click)
+  var buttonElement = event.target;
+
+  // what if a user clicks somerthing that is not an answer?
+  if (!buttonElement.matches('.choice')) {
+    return;
+  }
+
+  // check if user answered wrong
+  if (buttonElement.value !== Questions[Questiontrack].Answer) {
+    seconds -= 15
+
+    if (seconds < 0) {
+      seconds = 0
+    }
+
+    timer.textContent = seconds
+    Incorrect()
+  } else {
+    Correct()
+  }
+
+  Questiontrack++
+
+  if (seconds <= 0 || Questiontrack === Questions.length) {
+    endGame()
+  } else {
+    showQuestions()
+  }
+}
+
+choices.onclick = answerClicked;
+
+function endGame() {
+ clearInterval(timerId)
+
+  endScreen.removeAttribute('class')
+
+  finalScore.textContent = seconds
+  console.log(timer)
+  revealQuestion.setAttribute('class', 'hide')
+}
 //Correct answer
 function Correct () {
   feedback.textContent = "Correct!"
@@ -86,4 +143,4 @@ function Incorrect () {
 //End screen shows
 //Set final time as score + show on screen
 //Allow user to enter initials add score as highscore
-//Set highscores on the highscore page
+//Set highscores on the highscore page 
